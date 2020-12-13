25d1f12f2b84b4a7d6240f4c08037db6b528c490
Matheus Filipe <mfcr031@gmail.com>
2020-12-13 17:54:07 -0300

    update

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,105 +1,151 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
+import { addMonths, format, subMonths } from 'date-fns';
 
 import { useAuth } from '../hooks/auth';
 import api from '../services/api';
+import { IAppointmentExtraDailyInfo } from './dtos/IAppointmentsDailyInfo';
 
-const data = [
+type IData = Array<
   {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    'semana 1': 2780,
-    'semana 2': 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    'semana 1': 1890,
-    'semana 2': 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+    name: string;
+    week_1: number;
+    week_2: number;
+    week_3: number;
+    week_4: number;
+    week_5: number;
+    week_6: number;
+  } & {
+    [key: string]: number;
+  }
+>;
 
 export default function Dashboard(): JSX.Element {
   const { signOut, user } = useAuth();
-  const [appointmentsInfo, setAppointmentsInfo] = useState();
+
+  const [appointmentsInfo, setAppointmentsInfo] = useState<IAppointmentExtraDailyInfo>();
+  const [data, setData] = useState<IData>([]);
+  const [date, setDate] = useState(new Date());
 
   useEffect(() => {
     if (user) {
       api
-        .get(`appointments/info/${user.id}`, {
+        .get(`appointments/daily/info/${user.id}`, {
           params: {
-            month: new Date().getMonth() + 1,
-            year: new Date().getFullYear(),
+            month: date.getMonth() + 1,
+            year: date.getFullYear(),
           },
         })
         .then((response) => setAppointmentsInfo(response.data));
     }
-  }, [user]);
+  }, [user, date]);
+
+  useEffect(() => {
+    const updatedData = [
+      {
+        name: 'monday',
+        week_1: 0,
+        week_2: 0,
+        week_3: 0,
+        week_4: 0,
+        week_5: 0,
+        week_6: 0,
+      },
+      {
+        name: 'tuesday',
+        week_1: 0,
+        week_2: 0,
+        week_3: 0,
+        week_4: 0,
+        week_5: 0,
+        week_6: 0,
+      },
+      {
+        name: 'wednesday',
+        week_1: 0,
+        week_2: 0,
+        week_3: 0,
+        week_4: 0,
+        week_5: 0,
+        week_6: 0,
+      },
+      {
+        name: 'thursday',
+        week_1: 0,
+        week_2: 0,
+        week_3: 0,
+        week_4: 0,
+        week_5: 0,
+        week_6: 0,
+      },
+      {
+        name: 'friday',
+        week_1: 0,
+        week_2: 0,
+        week_3: 0,
+        week_4: 0,
+        week_5: 0,
+        week_6: 0,
+      },
+      {
+        name: 'saturday',
+        week_1: 0,
+        week_2: 0,
+        week_3: 0,
+        week_4: 0,
+        week_5: 0,
+        week_6: 0,
+      },
+    ] as IData;
+
+    updatedData.forEach((day) => {
+      for (let index = 1; index <= 6; index++) {
+        const weekName = `week_${index}`;
+
+        if (appointmentsInfo && appointmentsInfo[weekName]) {
+          day[weekName] = appointmentsInfo[weekName][day.name].clients;
+        }
+      }
+    });
+
+    setData(updatedData);
+  }, [appointmentsInfo]);
 
   return (
     <>
-      <h1>Hello Dashboard</h1>
-      <button onClick={signOut}>Logout</button>
+      {data && (
+        <>
+          <h1>Hello Dashboard</h1>
+          <button onClick={signOut}>Logout</button>
 
-      {console.log(appointmentsInfo)}
+          {/* TODO: Fazer esse botão flutuar */}
+          <div>
+            <button onClick={() => setDate(subMonths(date, 1))}>{'<'}</button>
+            <h1>{format(date, 'MMMM')}</h1>
+            <button onClick={() => setDate(addMonths(date, 1))}>{'>'}</button>
+          </div>
 
-      {/* <LineChart
-        width={600}
-        height={300}
-        data={data}
-        margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
-      >
-        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-      </LineChart> */}
-
-      <LineChart
-        width={730}
-        height={250}
-        data={data}
-        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="semana 1" stroke="#8884d8" />
-        <Line type="monotone" dataKey="semana 2" stroke="#82ca9d" />
-      </LineChart>
+          {/* TODO: resolver o seguinte erro: Received NaN for the `y1` attribute */}
+          <LineChart
+            width={730}
+            height={400}
+            data={data}
+            margin={{ top: 20, right: 30, left: 20, bottom: 20 }}
+          >
+            <CartesianGrid strokeDasharray="5 5" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="week_1" stroke="#8884d8" />
+            <Line type="monotone" dataKey="week_2" stroke="#82ca9d" />
+            <Line type="monotone" dataKey="week_3" stroke="#82c8ca" />
+            <Line type="monotone" dataKey="week_4" stroke="#82a1ca" />
+            <Line type="monotone" dataKey="week_5" stroke="#ca82c6" />
+            <Line type="monotone" dataKey="week_6" stroke="#ca828e" />
+          </LineChart>
+        </>
+      )}
     </>
   );
 }
---
+++ src/pages/dtos/IAppointmentsDailyInfo.ts
@@ -0,0 +1,27 @@
+type IServices = Array<{
+  description: string;
+  quantity: number;
+}>;
+
+interface IDayOfWeekInfo {
+  clients: number;
+  date: Date;
+  services: IServices;
+}
+
+type IWeekInfo = {
+  [key: string]: IDayOfWeekInfo;
+} & {
+  profitWithoutAdditionals: number;
+  profitWithAdditionals: number;
+};
+
+interface IAppointmentsDailyInfo {
+  [key: string]: IWeekInfo;
+}
+
+export type IAppointmentExtraDailyInfo = IAppointmentsDailyInfo & {
+  totalClientsInMonth: number;
+  totalProfitInMonthWithoutAdditionals: number;
+  totalProfitInMonthWithAdditionals: number;
+};
